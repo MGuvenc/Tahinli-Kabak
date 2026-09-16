fa3c964e4662350704d6addc3eeffbd278c818a2
Murat Güvenç <muratguvenc.py@gmail.com>
2026-09-17 02:19:26 +0300

    Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
@@ -12,5 +13,6 @@ createRoot(document.getElementById('root')!).render(
 				<App />
 			</AppProviders>
 		</BrowserRouter>
+		<Analytics />
 	</StrictMode>,
 );
