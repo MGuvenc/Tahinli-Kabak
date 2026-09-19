ff15cb79b0bfbaa268105ebdbc67017e7ec29f53
Murat Güvenç <muratguvenc.py@gmail.com>
2026-09-19 13:33:13 +0300

    Comment Sort

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -118,9 +118,9 @@ export default function Home() {
             <PostCard
               key={post.id}
               post={post}
+              commentCount={commentCounts.get(post.id) ?? 0}
               index={index}
-              variant={index === 0 ? 'featured' : 'default'}
-              commentCount={commentCounts.get(post.id) ?? 0} />
+              variant={index === 0 ? 'featured' : 'default'} />
 
             )}
 						</div> :
@@ -152,8 +152,7 @@ export default function Home() {
           popularPosts.length > 0 ?
           <div data-ev-id="ev_b2a2e766d2" className="grid grid-cols-1 md:grid-cols-2 gap-6">
 							{popularPosts.map((post, index) =>
-            <PostCard key={post.id} post={post} index={index}
-              commentCount={commentCounts.get(post.id) ?? 0} />
+            <PostCard key={post.id} post={post} commentCount={commentCounts.get(post.id) ?? 0} index={index} />
             )}
 						</div> :
 
@@ -184,8 +183,7 @@ export default function Home() {
           anonymousPosts.length > 0 ?
           <div data-ev-id="ev_5e10f033f6" className="grid grid-cols-1 md:grid-cols-3 gap-6">
 							{anonymousPosts.map((post, index) =>
-            <PostCard key={post.id} post={post} index={index}
-              commentCount={commentCounts.get(post.id) ?? 0} />
+            <PostCard key={post.id} post={post} commentCount={commentCounts.get(post.id) ?? 0} index={index} />
             )}
 						</div> :
 
